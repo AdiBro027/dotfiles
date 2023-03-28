0849895ccbcc6785f4c01fa82a282776fbddc439
Aditya Gautam <66688960+AdiBro027@users.noreply.github.com>
2023-03-28 15:50:27 +0530

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,11 @@ polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 # polybar left-workspace 2>&1 | tee -a /tmp/polybar.log & disown
-polybar left-workspace &
-polybar bar2 &
-polybar bar3 &
-# polybar main &
+# polybar left-workspace &
+# polybar bar2 &
+# polybar bar3 &
+polybar main &
+polybar time &
+polybar vol &
+polybar cpu &
+polybar memory &
